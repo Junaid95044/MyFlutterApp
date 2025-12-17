bd0f4aaf21f87fdf30db3565761839677a71e0f2
Junaid95044 <m.junaidarshad95@gmail.com>
2025-12-17 21:55:34 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    // Nothing else is needed here for Flutter UI
+}
